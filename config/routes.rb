d22a753901eb11b7a315f9936676aac3f7df654e
Sean <gilmore.seven@gmail.com>
2017-03-05 18:06:31 +1100

    add comments as sub resource of articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@
 Rails.application.routes.draw do
 
   # resource method allows us to declare a RESTful resource
-  resources :articles
+  resources :articles do
+    #adds a route into articles to see comments
+    #comments is a nested resource within articles
+    resources :comments
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
